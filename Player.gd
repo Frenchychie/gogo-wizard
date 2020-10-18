8331e107c4d0af48bcac67f1882cda22374e39b4
grantopher <shadowcarson@gmail.com>
2020-10-17 23:37:14 -0700

    Revert "doint stuff"
    This reverts commit 2a48c366a77801c7133a97bf36b5398cca7f0ee0.

--- Player.gd
+++ Player.gd
@@ -18,65 +18,65 @@ var combo_level = 0
 var can_combo = false
 
 func _ready():
-    state_machine = $AnimationTree.get("parameters/playback")
+	state_machine = $AnimationTree.get("parameters/playback")
 
 func _physics_process(delta):
-    
-    motion.y += GRAVITY
-    if motion.y > MAXFALLSPEED:
-        motion.y = MAXFALLSPEED
-    
-    if facing_right == true:
-        $Sprite.scale.x = 1
-    else:
-        $Sprite.scale.x = -1
-    
-    motion.x = clamp(motion.x,-MAXSPEED,MAXSPEED)
-    
-    if Input.is_action_pressed("right") && attacking == false:
-        motion.x += ACCEL
-        facing_right = true
-        if is_on_floor():
-            state_machine.travel("Walking")
-    elif Input. is_action_pressed("left") && attacking == false:
-        motion.x += -ACCEL
-        facing_right = false
-        if is_on_floor():
-            state_machine.travel("Walking")
-    else:
-        motion.x = lerp(motion.x,0,0.2)
-        if is_on_floor() && (motion.x != 0 || motion.y != 0) && action == false:
-            state_machine.travel("Idle")
-    if Input.is_action_just_pressed("jump") && attacking == false:
-        if jump_count < 1:
-            jump_count = jump_count + 1 
-            motion.y = JUMPFORCE
-            state_machine.travel("Jump")
-        print(jump_count)
-    if !is_on_floor():
-        on_ground = false
-        if motion.y > 0:
-            state_machine.travel("Falling")
-    else:
-        on_ground = true
-        jump_count = 0
-    if Input.is_action_pressed("attack"):
-            if on_ground == true && !attacking:
-                attacking = true
-                print("good job")
-                state_machine.travel("Attaccc")
-            elif can_combo == true:
-                print (can_combo)
-                state_machine.travel("Attacc 2")
-    motion = move_and_slide(motion, UP)
-        
+	
+	motion.y += GRAVITY
+	if motion.y > MAXFALLSPEED:
+		motion.y = MAXFALLSPEED
+	
+	if facing_right == true:
+		$Sprite.scale.x = 1
+	else:
+		$Sprite.scale.x = -1
+	
+	motion.x = clamp(motion.x,-MAXSPEED,MAXSPEED)
+	
+	if Input.is_action_pressed("right") && attacking == false:
+		motion.x += ACCEL
+		facing_right = true
+		if is_on_floor():
+			state_machine.travel("Walking")
+	elif Input. is_action_pressed("left") && attacking == false:
+		motion.x += -ACCEL
+		facing_right = false
+		if is_on_floor():
+			state_machine.travel("Walking")
+	else:
+		motion.x = lerp(motion.x,0,0.2)
+		if is_on_floor() && (motion.x != 0 || motion.y != 0) && action == false:
+			state_machine.travel("Idle")
+	if Input.is_action_just_pressed("jump") && attacking == false:
+		if jump_count < 1:
+			jump_count = jump_count + 1 
+			motion.y = JUMPFORCE
+			state_machine.travel("Jump")
+		print(jump_count)
+	if !is_on_floor():
+		on_ground = false
+		if motion.y > 0:
+			state_machine.travel("Falling")
+	else:
+		on_ground = true
+		jump_count = 0
+	if Input.is_action_pressed("attack"):
+			if on_ground == true && !attacking:
+				attacking = true
+				print("good job")
+				state_machine.travel("Attaccc")
+			elif can_combo == true:
+				print (can_combo)
+				state_machine.travel("Attacc 2")
+	motion = move_and_slide(motion, UP)
+		
 
 func start_combo_window():
-        can_combo = true
-    
+		can_combo = true
+	
 
 func attack_complete():
-    attacking = false
+	attacking = false
 
 
 
